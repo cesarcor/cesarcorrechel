61ccf302718c953e0c8d28fb50334b010ff956a7
cesarcor <cesarcorrechel@gmail.com>
2025-04-22 15:41:59 -0600

    incorporate header + hero

--- app/page.js
+++ app/page.js
@@ -1,5 +1,14 @@
 import styles from './page.module.css';
+import { Header } from '../components/header/header.js';
+import { Hero } from '../components/hero/hero.js';
 
 export default function Home() {
-	return <div className={styles.page}></div>;
+	return (
+		<div className={styles.page}>
+			<Header />
+			<main className={styles.main}>
+				<Hero />
+			</main>
+		</div>
+	);
 }
